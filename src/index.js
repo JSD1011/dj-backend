c096f7a3dc81a6f26eeba28024b9e13d22cf0b7a
JSD1011 <dabhijaymin123@gmail.com>
2025-06-25 17:59:42 +0530

    connection using two ways

---
+++ src/index.js
@@ -0,0 +1,70 @@
+// require('dotenv').config({path: './env'})
+
+// "dev" : "nodemon -r dotenv/config --experimental-json-modules  src/index.js"
+
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path : './env'
+})
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants";
+import express from 'express'
+const app = express()
+
+( async ()=>{
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+
+        app.on("error",(error)=>{
+            console.log("ERROR from App",error)
+            throw error
+        })
+
+        app.listen(process.env.PORT, ()=>{
+            console.log(`app is listening on port ${process.env.PORT}`);
+            
+        })
+
+    } catch (error) {
+        console.error("Error: from connection catch ..",error)
+        throw err
+
+    }
+})()
+*/
